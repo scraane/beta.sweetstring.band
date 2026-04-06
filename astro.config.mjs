9c2167207e2d86172e4e44d975f080a5cf4b7fd4
scraane <stephan@nexed.tech>
2026-04-06 08:45:41 +0800

    modified:   astro.config.mjs 	modified:   package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
-
-import cloudflare from '@astrojs/cloudflare';
-
 export default defineConfig({
-  output: 'server',
-  adapter: cloudflare(),
+  output: 'static',
 });
