cc656a979f1733801bf8de7ba5e90b4aaccdf576
scraane <stephan@nexed.tech>
2026-04-06 08:42:12 +0800

    modified:   astro.config.mjs 	modified:   package.json 	new file:   wrangler.toml

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,3 +3,10 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({});
+
+import cloudflare from '@astrojs/cloudflare';
+
+export default defineConfig({
+  output: 'server',
+  adapter: cloudflare(),
+});
